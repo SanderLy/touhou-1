83f0b0ac07aa3381d3f6d90681d03eaad49ab319
Lysander Adriel Gawaran <glysander22@gmail.com>
2016-09-04 09:19:33 +0800

    added mp animation

--- game.py
+++ game.py
@@ -23,6 +23,8 @@ sprites_list = pygame.sprite.Group()
 collide_list = pygame.sprite.Group()
 
 #instantiate the sprites
+hp = pygame.image.load('hp_bar/marisa_health.png').convert_alpha()
+mp = pygame.image.load('hp_bar/mp.png').convert_alpha()
 marisa = Marisa('marisa',332,345,0.05, 10,'character')
 mamizou = Mamizou('mamizou',1,15,0.09,10,'character')
 
@@ -47,21 +49,40 @@ rate = 300
 click_event = 0
 mob_rate = 300
 
-resource = 500
+resource = 5000
+
 #---------------------------------
 
+#mob indices
+type_mob = {1:(0,1),2:(2,3),3:(4,5)}
+current_mob = 1
+
+
 #printing of label
-font = pygame.font.Font(None, 50)
-lblRes = font.render(str(resource),1,(0,0,0))
+font = pygame.font.Font(None, 35)
 
 marisa_animation.play()
 mainClock = pygame.time.Clock()
 BGCOLOR = (100, 50, 50)
 cooldown = 1
 
+#---------------------------------
+
+#try animationn of mp bar
+MPx =122
+MPy = 44
+mp_rect = mp.get_rect()
+x = 226
 while True:
     windowSurface.fill(BGCOLOR)
     now = pygame.time.get_ticks()
+    lblRes = font.render(str(resource),1,(0,0,0))# show lable
+
+    #changing of mp bar
+    chop_rect = (0,0,x,0)
+    crop_mp = pygame.transform.chop(mp,chop_rect)
+    
+
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
@@ -71,8 +92,19 @@ while True:
                 marisa_animation.pause()
                 marisa_animation = marisa_idle
                 marisa_animation.play()
+        if event.type == KEYDOWN: #  changing the type of mob     
+            if event.key == K_RIGHT:
+                current_mob+=1
+                if current_mob > 3: 
+                    current_mob = 1 
 
-
+            if event.key == K_LEFT:
+                current_mob-=1
+                if current_mob < 1 : 
+                    current_mob = 3
+    mob_lower, mob_upper = type_mob[current_mob]
+   
+    
     keys = pygame.key.get_pressed()  #checking pressed keys
     mouse = pygame.mouse.get_pressed()
 
@@ -91,7 +123,7 @@ while True:
         if mamizou.rect.y > 0:
             mamizou.rect.y-=10
     if keys[pygame.K_DOWN]:
-        if mamizou.rect.y < 430:
+        if mamizou.rect.y < 530:
             mamizou.rect.y+=10
     if keys[pygame.K_a]:
         if marisa.rect.x>0:
@@ -118,12 +150,14 @@ while True:
             press_event = pygame.time.get_ticks()
     if mouse[0]:
         if now - click_event >= mob_rate:
-            mob = Mob('mob',0,1,0.85,3,'mob')
-            sprites_list.add(mob)
-            click_event = now
+            if resource > 1000: #1000 is the cost of the mob
+                mob = Mob('mob',mob_lower,mob_upper,0.25,3,'mob')
+                sprites_list.add(mob)
+                click_event = now
+                resource -= 500
 
 
-
+    resource+=5
     projectile_list.update()
     sprites_list.update()
     collide_list.update()
@@ -135,7 +169,10 @@ while True:
         if sprite.ctype == 'character' or sprite.ctype == 'mob':
             sprite.hp -= 1
             if sprite.hp <= 0:
-               sprite.kill()
+                x-=20
+                if x < 0:
+                    x = 226
+                sprite.kill()
                 #print sprite.hp
 
     for projectile in projectile_list:
@@ -153,6 +190,8 @@ while True:
     
     projectile_list.draw(windowSurface)
     sprites_list.draw(windowSurface)
+    windowSurface.blit(hp,(0,0))
+    windowSurface.blit(crop_mp,(MPx,MPy)) #122 and 44
     windowSurface.blit(lblRes,(450,50))
     pygame.display.flip()
     pygame.display.update()

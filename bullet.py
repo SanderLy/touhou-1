b0849501eead6f9c1cfcfe80cf3426b9ff446b93
tim-calilao <calilao20@gmail.com>
2016-09-04 14:57:39 +0800

    bullet has no an dmg attribute

--- bullet.py
+++ bullet.py
@@ -1,11 +1,12 @@
 import pygame
 class Bullet(pygame.sprite.Sprite):
-	def __init__(self,fname,btype):
+	def __init__(self,fname,btype, dmg = 1):
 		super(Bullet,self).__init__()
 		self.fname = fname
 		self.image = pygame.image.load('gifs/'+self.fname+'/projectile.png').convert_alpha()
 		self.rect = self.image.get_rect()
 		self.btype = btype
+		self.dmg = dmg
 
 	def update(self):
 		if self.btype == 'character':
--- game.py
+++ game.py
@@ -146,7 +146,7 @@ while True:
     if keys[pygame.K_j]:
         if now - press_event >= rate:
             if marisa.alive():
-                bullet = Bullet('marisa','character')
+                bullet = Bullet('marisa','character',5)
                 bullet.rect.x = marisa.rect.x+76
                 bullet.rect.y = marisa.rect.y+25
                 projectile_list.add(bullet)
@@ -203,14 +203,16 @@ while True:
     sprites_list.update()
     collide_list.update()
 
-    #returns a dictionary{[sprites_list]:[projectile_list]}
+    
+    #sets damage of colliding projectile
     collide_list2 = pygame.sprite.groupcollide(projectile_list, sprites_list, False, False)
     for projectile in collide_list2:
         if projectile.btype == 'character':
-            dmg = 5
+            dmg = projectile.dmg
 
-
+    #returns a dictionary{[sprites_list]:[projectile_list]}
     collide_list = pygame.sprite.groupcollide(sprites_list, projectile_list, False, True)
+
     #check sprite if character or mob
     #checks if a sprite hits 0 hp per collide
     for sprite in collide_list:
